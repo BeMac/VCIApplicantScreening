a57e008728b7fe43eccf3b9c61c28e135829386b
Brian McLaughlin <bmclaughlin@verifiedcredentials.com>
2015-11-06 09:30:56 -0600

    Initial Commit

---
+++ ScreeningApp/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ScreeningApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            //ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+
+            int originalNumber = 1;
+
+            object boxedObject = originalNumber;
+            boxedObject = 123;
+            
+            
+            //unbox this variable;
+            //originalNumber = (int) boxedObject;
+            
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
