89ee09211530bc3af1d9f457232e3a95d0594397
Brian McLaughlin <bmclaughlin@verifiedcredentials.com>
2015-11-11 14:28:42 -0600

    Something got broken here.

---
+++ ScreeningApp/Models/CSharpModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace ScreeningApp.Models
+{
+    public enum CurrencyType
+    {
+        [Description("U.S. Dollars")]
+        USD = 1,
+        [Description("Belarusian Rubles")]
+        BYR = 2,
+        [Description("Kenyan Shillings")]
+        KES = 3,
+        [Description("Brazilian Real")]
+        BRL = 4,
+        [Description("Chinese Yuan")]
+        CNY = 5,
+    }
+}
